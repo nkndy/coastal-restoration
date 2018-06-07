0c94f870e5f63af4d086663dfdaf111e83e9a7e2
_nickkennedy <nicholas.stuart.kennedy@gmail.com>
2018-06-07 11:25:04 -0700

    Updating coastal-restoration.js

--- coastal-restoration.js
+++ coastal-restoration.js
@@ -11,8 +11,6 @@ switch (ctx.body['metadata[plan]']) {
         plan = "no_plan";
 }
 
-console.log(plan)
-
 // Create a new customer and then a new charge for that customer:
 stripe.customers.create({
   email: ctx.body.email,
@@ -39,11 +37,27 @@ stripe.customers.create({
   console.log("did it")
   // Send an email:
   var client = new postmark.Client("0a071725-2b2e-4afd-9fde-88c913798371");
-  client.sendEmail({
-    "From": "info@coastrestore.com",
-    "To": "info@coastrestore.com",
-    "Subject": "Test",
-    "TextBody": "Hello from Postmark!"
+  client.sendEmailWithTemplate({
+    "From": "sender@example.com",
+    "To": "recipient@example.com",
+    "TemplateId": 6826302,
+    "TemplateModel": {
+      "product_name": "product_name_Value",
+      "name": "name_Value",
+      "product_url": "product_url_Value",
+      "action_url": "action_url_Value",
+      "login_url": "login_url_Value",
+      "username": "username_Value",
+      "trial_length": "trial_length_Value",
+      "trial_start_date": "trial_start_date_Value",
+      "trial_end_date": "trial_end_date_Value",
+      "support_email": "support_email_Value",
+      "live_chat_url": "live_chat_url_Value",
+      "sender_name": "sender_name_Value",
+      "help_url": "help_url_Value",
+      "company_name": "company_name_Value",
+      "company_address": "company_address_Value"
+    }
   });
 }).catch(function(err) {
   callback(err);
