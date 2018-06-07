27a14ce0ff2762cb57ae77a646d27282d2c48c69
_nickkennedy <nicholas.stuart.kennedy@gmail.com>
2018-06-07 11:25:36 -0700

    Updating coastal-restoration.js

--- coastal-restoration.js
+++ coastal-restoration.js
@@ -34,7 +34,6 @@ stripe.customers.create({
   }, callback);
 }).then(function(subscription) {
   // New subscription created on a new customer
-  console.log("did it")
   // Send an email:
   var client = new postmark.Client("0a071725-2b2e-4afd-9fde-88c913798371");
   client.sendEmailWithTemplate({
@@ -64,33 +63,3 @@ stripe.customers.create({
   // Deal with an error
 });
 }
-
-/////////////// V3 /////////////
-// module.exports = function(ctx, callback) {
-// var stripe = require('stripe@6.0.0')(ctx.secrets.STRIPE_SK_TEST);
-// // Create a new customer and then a new charge for that customer:
-// stripe.customers.create({
-//   email: ctx.body.email,
-//   metadata: {
-//       'name': ctx.body['metadata[name]'],
-//       'company_name': ctx.body['metadata[company_name]'],
-//       'phone': ctx.body['metadata[phone]']
-//   }
-// }).then(function(customer){
-//   return stripe.customers.createSource(customer.id, {
-//     source: ctx.body['stripeToken[id]']
-//   });
-// }).then(function(source) {
-//   return stripe.charges.create({
-//     amount: 1900,
-//     currency: 'cad',
-//     customer: source.customer
-//   }, callback);
-// }).then(function(charge) {
-//   // New charge created on a new customer
-//   console.log("did it")
-// }).catch(function(err) {
-//   callback(err);
-//   // Deal with an error
-// });
-// }
