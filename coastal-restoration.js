fd6f1dd434159ca3b54e900b45cab6d1d377b40a
_nickkennedy <nicholas.stuart.kennedy@gmail.com>
2018-07-05 21:48:11 -0700

    Updating coastal-restoration.js

--- coastal-restoration.js
+++ coastal-restoration.js
@@ -80,21 +80,13 @@ stripe.customers.create({
     "To": ctx.body.email,
     "TemplateId": 6826302,
     "TemplateModel": {
-      "product_name": "product_name_Value",
+      "product_name": "The Clayoquot Cleanup",
       "name": ctx.body['metadata[name]',
-      "product_url": "product_url_Value",
-      "action_url": "action_url_Value",
-      "login_url": "login_url_Value",
-      "username": "username_Value",
-      "trial_length": "trial_length_Value",
-      "trial_start_date": "trial_start_date_Value",
-      "trial_end_date": "trial_end_date_Value",
-      "support_email": "support_email_Value",
-      "live_chat_url": "live_chat_url_Value",
-      "sender_name": "sender_name_Value",
-      "help_url": "help_url_Value",
-      "company_name": "company_name_Value",
-      "company_address": "company_address_Value"
+      "support_email": "info@coastrestore.com",
+      "sender_name": "Josh Temple",
+      "help_url": "https://www.clayoquotcleanup.com/",
+      "company_name": "Clayoquot Cleanup",
+      "company_address": "Tofino, BC"
     }
   }, function(error, result) {
     if(error) {
