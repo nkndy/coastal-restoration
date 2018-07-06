70aacfdec460f6fb105abaef72018cb8bd5652b5
_nickkennedy <nicholas.stuart.kennedy@gmail.com>
2018-07-05 21:12:05 -0700

    Updating coastal-restoration.js

--- coastal-restoration.js
+++ coastal-restoration.js
@@ -51,6 +51,7 @@ stripe.customers.create({
 // If subscription is annual
 // Create a new customer and then a new charge for that customer:
 if (ctx.body['metadata[subscriptionType]'] == "Annual") {
+console.log("annual")
 stripe.customers.create({
   email: ctx.body.email,
   metadata: {
